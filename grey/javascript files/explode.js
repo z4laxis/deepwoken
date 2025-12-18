e5fb31ede29382b1931ac4fe06491aed82770f08
zalaxis <130976751+z4laxis@users.noreply.github.com>
2025-12-18 21:25:35 +0100

    Deleted /grey/, added the neocities website on the main menu

--- grey/javascript files/explode.js
+++
@@ -1,9 +0,0 @@
-function explode(victim){
-    document.getElementById(victim).style.display = 'none';
-}
-
-function appear(explosion){
-    document.getElementById(explosion).style.display = 'block';
-
-    setTimeout(function(){ document.getElementById(explosion).style.display = 'none' }, 780)
-}
--- grey/javascript files/hideandshow.js
+++
@@ -1,7 +0,0 @@
-function showdiv(div){
-    document.getElementById(div).style.display = 'block';
-}
-
-function hidediv(div){
-    document.getElementById(div).style.display = 'none';
-}
