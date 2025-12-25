9a4baa42bf620aa6d430435d3d20dea7bd825201
zalaxis <130976751+z4laxis@users.noreply.github.com>
2025-12-25 13:59:23 +0100

    Linked blacksmith interface with outfits api

--- assets/js/blacksmith.js
+++ assets/js/blacksmith.js
@@ -1,8 +1,21 @@
 document.querySelectorAll('.panel-item').forEach(item => {
     item.addEventListener('click', () => {
-        document.querySelectorAll('.panel-item').forEach(i => i.dataset.active = "false");
+        document.querySelectorAll('.panel-item').forEach(i => {
+            i.dataset.active = "false";
+        });
         item.dataset.active = "true";
-        
-        document.getElementById("iframe").src= `/assets/iframes/blacksmith?outfit=${item.id}`
+
+        document.getElementById("iframe").src = `/assets/iframes/blacksmith?outfit=${item.id}`;
+
+        fetch('/assets/json/outfits.json')
+            .then(res => res.json())
+            .then(outfits => {
+                const outfit = outfits.find(o => o.id === item.id);
+
+                console.log("Outfit :", outfit);
+            })
+            .catch(err => {
+                console.error("Error:", err);
+            });
     });
 });
