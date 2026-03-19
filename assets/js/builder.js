f2ef0815b33785b04f29137539795f4bb411ff84
zalaxis <imnotchxvi@gmail.com>
2026-03-19 13:03:23 +0000

    Updated builder selection systems

---
+++ assets/js/builder.js
@@ -0,0 +1,11 @@
+const selectedAspect = document.getElementById('aspects').value;
+
+function select() {
+    fetch('/assets/json/builder.json')
+        .then(res => res.json())
+        .then(aspects => {
+
+            console.log(aspects[selectedAspect]);
+        })
+        .catch(err => console.error(err));
+}
