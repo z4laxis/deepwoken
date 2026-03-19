50eb9b7791f9e77610fab4a150a6d6a938cb5cb9
zalaxis <imnotchxvi@gmail.com>
2026-03-19 13:13:13 +0000

    Updated aspects width alternation on the journal selection inputs

--- assets/js/builder.js
+++ assets/js/builder.js
@@ -1,6 +1,5 @@
-const selectedAspect = document.getElementById('aspects').value;
-
 function select() {
+    const selectedAspect = document.getElementById('aspects').value;
     fetch('/assets/json/builder.json')
         .then(res => res.json())
         .then(aspects => {
